3073fbf8ea4d86e2484b143c073b7d7c17dc217d
Maria Paula Chavez <mariapaulachavez@MacBook-Air-de-Maria-9.local>
2025-10-24 12:59:26 -0500

    1

--- src/model/Incident.java
+++ src/model/Incident.java
@@ -1,5 +1,51 @@
-package model;
 
-public class Incident {
+package model;
+import java.time.LocalDate;
+
+public class Incident { 
+
+    private LocalDate dateReport; 
+    private String descripcion; 
+    private boolean solucion; 
+    private int solucionHoras;
+
+    public incident (LocalDate dateReport, String descripcion) {
+        this.dateReport = dateReport; 
+        this.descripcion = descripcion; 
+    }
+
+    public LocalDate getDateReport() {
+        return dateReport;
+    }
+
+    public String getDescripcion () {
+        return descripcion;
+    }
+
+    public boolean getSolucion () {
+        return solucion;
+    }
+
+    public int getSolucionHoras () {
+        return solutionHoras;
+    }
+
+    public void setDateReport(LocalDate dateReport) {
+        this.DateReport = dateReport; 
+    }
+
+    public void setDescripcion(String descriptcion) {
+        this.Descripcion = descripcion; 
+    }
+
+    public void setSolucion(boolean solucion){
+        this.Solucion = solucion;
+    }
+
+    public void setSolutionHours(int solucionHours) {
+        this.SolucionHours = solucionHours;
+    }
+}
+﻿
     
 }
